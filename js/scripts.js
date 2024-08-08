dbc409cfe0082b393101223b77db753169a9d5b2
sophiefau <sophiefauquembergue@gmail.com>
2024-08-08 16:55:18 +0200

    create pokemon list in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-alert('Hello world');
-
-let favoriteFood = 'lasagna';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Pikachu', height: 0.4, type: 'Electric'},
+  {name: 'Charmander', height: 0.6, type: 'Fire'},
+  {name: 'Vulpix', height: 0.6, type: 'Fire'},
+  {name: 'Meowth', height: 0.4, type: 'Normal'},
+  {name: 'Snorlax', height: 2.1, type: 'Normal'},
+  {name: 'Foongus', height: 0.2, type: ['Grass','Poison',]},
+  {name: 'Purrloin', height: 0.4, type: 'Dark'}
+];
